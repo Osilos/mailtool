2d4f8414e81c6ade358d6a66967b30b2ece928ff
Osilos <f.marianacci@gmail.com>
2018-06-11 16:50:20 +0200

    work on edit template

--- src/common/components/templates/EditTemplateForm.js
+++ src/common/components/templates/EditTemplateForm.js
@@ -17,12 +17,24 @@ const chipsStyle = {
     cursor: 'move'
 };
 
+const movingChipStyle = {
+    position: 'absolute',
+    backgroundColor: 'green'
+};
+
+const movedChipStyle = {
+    backgroundColor: 'red'
+};
+
+const editStateEnum = ['IDLE', 'TEXT', 'INPUT'];
+
 class EditTemplateForm extends Component {
     constructor(props) {
         super();
 
         this.state = {
-            inputDrage: null
+            inputDrag: null,
+            editState: editStateEnum[0]
         };
 
         this.chipOnMouseUp = this.chipOnMouseUp.bind(this);
@@ -31,31 +43,73 @@ class EditTemplateForm extends Component {
         this.chipOnMouseMove = this.chipOnMouseMove.bind(this);
     }
 
-    generateContent(content) {
+    generateContentIdle(content) {
         const inputs = content.split('%%');
         let i = 0;
         return inputs.map(text => {
-            if (i++ % 2 == 0) {
-                const words = text.split(' ');
-                return words.map((word, index) => {
-                    return <span key={word + index}>{word} </span>;
-                });
+            if (this.isJson(text)) {
+                return this.getInputAsChip(text, i++);
             } else {
-                let parsedinput = JSON.parse(text);
-                if (parsedinput.type === 'space') return <br key={i} />;
-                return (
-                    <Chip
-                        style={chipsStyle}
-                        key={i}
-                        label={parsedinput.type + ' : ' + parsedinput.id}
-                        onMouseDown={this.chipOnMouseDown}
-                        onMouseUp={this.chipOnMouseUp}
-                        onMouseLeave={this.chipOnMouseLeave}
-                        onMouseMove={this.chipOnMouseMove}
-                    />
-                );
+                return <span key={i++}>{text} </span>;
             }
         });
+    }
+
+    generateContentInput(content) {
+        const inputs = content.split('%%');
+        let i = 0;
+        return inputs.map((text, index) => {
+            if (this.isJson(text)) {
+                const parsedInput = JSON.parse(text);
+                if (this.state.inputDrag.id == parsedInput.id) {
+                    return this.getInputAsMoved(text);
+                } else {
+                    return this.getInputAsChip(text, i++);
+                }
+            } else {
+                const words = text.split(' ');
+                return words.map(word => {
+                    return <span key={i++}>{word} </span>;
+                });
+            }
+        });
+    }
+
+    getInputAsMoved(input) {
+        let parsedinput = JSON.parse(input);
+        if (parsedinput.type === 'space') return <br key={index} />;
+        return (
+            <Chip
+                style={movedChipStyle}
+                key={'moved'}
+                label={parsedinput.type + ' : ' + parsedinput.id}
+                onMouseDown={this.chipOnMouseDown}
+                onMouseUp={this.chipOnMouseUp}
+                onMouseLeave={this.chipOnMouseLeave}
+                onMouseMove={this.chipOnMouseMove}
+            />
+        );
+    }
+
+    getInputAsChip(input, index = 0) {
+        let parsedinput = JSON.parse(input);
+        if (parsedinput.type === 'space') return <br key={index} />;
+        return (
+            <Chip
+                style={chipsStyle}
+                key={index}
+                id={parsedinput.id}
+                label={parsedinput.type + ' : ' + parsedinput.id}
+                onMouseDown={this.chipOnMouseDown}
+                onMouseUp={this.chipOnMouseUp}
+                onMouseLeave={this.chipOnMouseLeave}
+                onMouseMove={this.chipOnMouseMove}
+            />
+        );
+    }
+
+    isJson(text) {
+        return text[0] === '{' && text[text.length - 1] === '}';
     }
 
     chipOnMouseMove(e) {
@@ -71,7 +125,7 @@ class EditTemplateForm extends Component {
     chipOnMouseDown(e) {
         const chip =
             e.target.nodeName === 'DIV' ? e.target : e.target.parentElement;
-        this.setState({ inputDrag: chip });
+        this.setState({ inputDrag: chip, editState: editStateEnum[2] });
         chip.style.position = 'absolute';
         chip.style.left = e.clientX - 30 + 'px';
         chip.style.top = e.clientY - 15 + 'px';
@@ -97,7 +151,13 @@ class EditTemplateForm extends Component {
     }
 
     getContent(content) {
-        return this.generateContent(content);
+        console.log(this.state.editState);
+        if (this.state.editState == editStateEnum[0]) {
+            return this.generateContentIdle(content);
+        }
+        if (this.state.editState == editStateEnum[2]) {
+            return this.generateContentInput(content);
+        }
     }
 
     render() {
